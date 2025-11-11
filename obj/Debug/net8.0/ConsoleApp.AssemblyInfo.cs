624e89a12a43e9f19eb07ff02318425840cbcf84
Henning-1025 <sthe082025@gmail.com>
2025-11-11 14:34:58 +0100

    gitignore

--- obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99944beb103fb32bc6263058ec5a40572940ff47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b17166a3eca55b0fcc3143e87d752b50a46fefd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
